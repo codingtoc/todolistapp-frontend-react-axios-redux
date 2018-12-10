567455bea16f24631488d7a52b2979890f763018
codingtoc <codingtoc@gmail.com>
2018-12-10 22:09:39 +0900

    리덕스 connect 함수로 App 컴포넌트 연결(src/App.js 변경)

--- src/App.js
+++ src/App.js
@@ -3,18 +3,14 @@ import axios from "axios";
 import List from "./components/List";
 import InputTodo from "./components/InputTodo";
 import "./App.css";
+import { connect } from "react-redux";
+import { fetchTodoList } from "./actions";
 
 class App extends Component {
-  // 할일 목록 상태 데이터
-  state = {
-    todos: []
-  };
   // DB에서 할일 목록 가져와 상태 변경하기
   fetchTodos = () => {
     axios.get("http://localhost:4000/api/todos").then(res => {
-      this.setState({
-        todos: res.data
-      });
+      this.props.handlefetchTodoList(res.data);
     });
   };
   // 컴포넌트가 마운트되면 DB에서 할일 목록 가져와 상태 변경하기
@@ -29,7 +25,7 @@ class App extends Component {
   };
   // DB에서 기존 할일 완료여부 수정 후 할일 목록 가져와 상태 변경하기
   doneToggle = id => {
-    const todo = this.state.todos.find(todo => todo._id === id);
+    const todo = this.props.todos.find(todo => todo._id === id);
     axios
       .put("http://localhost:4000/api/todos/" + id, { done: !todo.done })
       .then(res => {
@@ -48,7 +44,7 @@ class App extends Component {
       <div className="container">
         <h1 className="center blue-text">Todo List App</h1>
         <List
-          todos={this.state.todos}
+          todos={this.props.todos}
           deleteTodo={this.deleteTodo}
           doneToggle={this.doneToggle}
         />
@@ -58,4 +54,23 @@ class App extends Component {
   }
 }
 
-export default App;
+// 컴포넌트의 props에 매핑할 상태 정의
+const mapStateToProps = state => {
+  return {
+    todos: state.todos
+  };
+};
+// 컴포넌트의 props에 매핑할 디스패처 정의
+const mapDispatchToProps = dispatch => {
+  return {
+    handlefetchTodoList: todos => {
+      dispatch(fetchTodoList(todos));
+    }
+  };
+};
+
+// connect 함수로 App 컴포넌트 연결
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
